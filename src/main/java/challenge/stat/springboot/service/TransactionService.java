b07754feafb48fcc1aa063c8884e9e3b0497c1f8
Wallyson Araujo <wallyjsa@gmail.com>
2025-10-22 08:47:23 -0300

    chore: create Transaction services with thread-safe

---
+++ src/main/java/challenge/stat/springboot/service/TransactionService.java
@@ -0,0 +1,30 @@
+package challenge.stat.springboot.service;
+
+import challenge.stat.springboot.model.Transaction;
+import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
+import java.util.DoubleSummaryStatistics;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+@Service
+public class TransactionService {
+    private final Queue<Transaction> transactions = new ConcurrentLinkedQueue<>();
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public void clearTransactions() {
+        transactions.clear();
+    }
+
+    public DoubleSummaryStatistics getStatistics() {
+        OffsetDateTime now = OffsetDateTime.now();
+        return transactions.stream()
+                .filter(t -> t.getDateTime().isAfter(now.minusSeconds(60)))
+                .mapToDouble(Transaction::getValue)
+                .summaryStatistics();
+    }
+}
